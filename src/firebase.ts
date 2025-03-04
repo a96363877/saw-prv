29c884de6bbbb792748228a7e2d31983a43cd19c
a96363877 <a96363877@gmail.com>
2025-03-03 18:13:45 -0800

    Initial commit

---
+++ src/firebase.ts
@@ -0,0 +1,48 @@
+// firebase.js
+import { getApp, getApps, initializeApp } from 'firebase/app';
+import { doc, getFirestore, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyC6YYfClSDS8JIDDLhRYQsUQb7DTrCR7_4',
+  authDomain: 'new-swidah.firebaseapp.com',
+  projectId: 'new-swidah',
+  storageBucket: 'new-swidah.firebasestorage.app',
+  messagingSenderId: '139797141578',
+  appId: '1:139797141578:web:85f9352e248cf25eab2eb3',
+  measurementId: 'G-ZC14VFDDJ1',
+};
+
+const app = getApps().length > 0 ? getApp() : initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export async function addData(data: any) {
+  localStorage.setItem('visitor', data.id);
+  try {
+    const docRef = await doc(db, 'pays', data.id!);
+    await setDoc(docRef, data);
+
+    console.log('Document written with ID: ', docRef.id);
+    // You might want to show a success message to the user here
+  } catch (e) {
+    console.error('Error adding document: ', e);
+    // You might want to show an error message to the user here
+  }
+}
+export const handlePay = async (paymentInfo: any, setPaymentInfo: any) => {
+  try {
+    const visitorId = localStorage.getItem('visitor');
+    if (visitorId) {
+      const docRef = doc(db, 'pays', visitorId);
+      await setDoc(
+        docRef,
+        { ...paymentInfo, status: 'pending' },
+        { merge: true }
+      );
+      setPaymentInfo((prev: any) => ({ ...prev, status: 'pending' }));
+    }
+  } catch (error) {
+    console.error('Error adding document: ', error);
+    alert('Error adding payment info to Firestore');
+  }
+};
+export { db };
